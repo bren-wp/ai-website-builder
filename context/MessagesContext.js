cde6df51e7d3487f51eb3814761b3f658563a166
Brendigo <info@brendigo.com>
2026-03-09 20:37:16 +0100

    Update MessagesContext.js

--- context/MessagesContext.js
+++ context/MessagesContext.js
@@ -1,3 +1,13 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
-export const MessagesContext = createContext();
+export const MessagesContext = createContext(null);
+
+export function useMessages() {
+  const context = useContext(MessagesContext);
+
+  if (context === null) {
+    throw new Error("useMessages must be used within a MessagesProvider.");
+  }
+
+  return context;
+}
